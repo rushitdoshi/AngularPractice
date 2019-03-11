f407034d5b0de2d9b74a9cc8f89971623e7e7746
Rushit Doshi <doshi.rs@gmail.com>
2019-03-10 19:24:05 -0700

    Added css style

--- 5 style component/app/event-details.component.ts
+++ 5 style component/app/event-details.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'event-details',
-  templateUrl: 'app/event-details.component.html'
+  templateUrl: 'app/event-details.component.html',
+  styleUrls: ['app/event-details.component.css']  
 })
 export class EventDetailsComponent {
   event = {name:'ngConf 2025', date: '3/1/2025', time: '8am', location: {address: '123 Main St', city: 'Salt Lake City, UT', country: 'USA'}}
