133145eab643af42f7fd62d6fbc4643b68d8c078
Rushit Doshi <doshi.rs@gmail.com>
2019-03-19 22:00:51 -0700

    ngClass with different return types

--- 11 ngClass/app/events-list.component.ts
+++ 11 ngClass/app/events-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'event-details',
@@ -18,5 +18,45 @@ export class EventsListComponent {
     {name:'ng-conf 2037', date: '4/15/2037', time: '9am', onlineUrl: 'https://www.ng-conf.org/', format:"Online"},
     {name:'Future Conf (Location/Url TBD)', date: '6/10/2037', time: '8am'},
     {name:'UN Angular Summit', date: '6/10/2037', time: '8am', location: {address: 'The UN Angular Center', city: 'New York', country: 'USA'}, format:"InPerson"}
-  ]
+  ];
+
+  
+  getTitleClass(eventFormat) {
+    
+    // Direct return big object
+    /*
+    return {
+      // single quotes are needed only when there is dash within variable name
+      'in-person': eventFormat === "InPerson",
+      online: eventFormat === "Online", 
+      tbd: eventFormat !== "InPerson" && eventFormat !== "Online"
+    };
+    */
+    
+    // Return object using constant boolean values
+    /*
+    const inPersonEvent = eventFormat === 'InPerson';
+    const onlineEvent = eventFormat === 'Online';
+    const tbdEvent = !inPersonEvent && !onlineEvent;
+    return {'in-person': inPersonEvent, online: onlineEvent, tbd: tbdEvent};
+    */
+    
+    // string return with if condition
+    /*
+    if (eventFormat === 'InPerson') {
+      return 'in-person';
+    } else if (eventFormat === 'Online') {
+      return 'online';
+    }
+    return 'tbd';
+    */
+    
+    // array return with if condition
+    if (eventFormat === 'InPerson') {
+      return ['in-person'];
+    } else if (eventFormat === 'Online') {
+      return ['online'];
+    }
+    return ['tbd'];
+  }  
 }
