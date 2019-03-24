85f2060ebe3fbbead602afd5bdd07683c14265d0
Rushit Doshi <doshi.rs@gmail.com>
2019-03-24 14:37:57 -0700

    Added sorting

--- 33 sorting data/app/parent.component.ts
+++ 33 sorting data/app/parent.component.ts
@@ -26,11 +26,19 @@ export class ParentComponent implements OnInit {
   }
   
   sortAsc() {
-    // implement
+    this.movies.sort((movie1, movie2) => {
+      if (movie1.rating > movie2.rating) return 1;
+      if (movie1.rating < movie2.rating) return -1;
+      return 0; 
+    });
   }
   
   sortDesc() {
-    // implement
+    this.movies.sort((movie1, movie2) => {
+      if (movie1.rating < movie2.rating) return 1;
+      if (movie1.rating > movie2.rating) return -1;
+      return 0; 
+    });
   }
   
 
