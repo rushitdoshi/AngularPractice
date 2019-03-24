b20115718b0fd773d94fb9e2d1ee4a3595d8121b
Rushit Doshi <doshi.rs@gmail.com>
2019-03-24 13:33:26 -0700

    Added multi-slot projection

--- 27 multiple slot content projection/app/multi-slot.component.ts
+++ 27 multiple slot content projection/app/multi-slot.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'multi-slot',
   template: `
-    Template Goes Here
+    <div class="well">
+      <div style="border:1px solid red">
+        <ng-content select="[slot1]"></ng-content>
+      </div>
+      <div style="border:1px solid blue">
+        <ng-content select="[slot2]"></ng-content>
+      </div>
+      <div style="border:1px solid green">
+        <ng-content select="[slot3]"></ng-content>
+      </div>
+    </div>
   `
 })
 export class MultiSlotComponent {
