33c1681fe9aeb788830b6d539e490bbb70418634
Rushit Doshi <doshi.rs@gmail.com>
2019-03-24 13:10:31 -0700

    Added content projection

--- 26 content projection/app/closable-well.component.ts
+++ 26 content projection/app/closable-well.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'closable-well',
   template: `
-    Template Goes Here
+    <div class="well" *ngIf="visible">
+      <ng-content></ng-content><br>
+      <button class="btn btn-primary" (click)="closeProjection()">Close</button>
+    </div>
   `
 })
 export class ClosableWellComponent {
 
-  
+  visible: boolean = true;
+
+  closeProjection() {
+    this.visible = false;
+  }
 }
